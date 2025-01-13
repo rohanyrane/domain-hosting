282cafb89385458f2b29a3d1c653946cb690b3fd
sakshi.dhenge <sakshi.dhenge@idfy.com>
2025-01-13 12:25:06 +0530

    test

--- 1.js
+++ 1.js
@@ -773,7 +773,7 @@ window.onload = () => {
 }
 
     // List of domains to block
-    const domainsToBlock = ["linkedin.com", "facebook.com", "twitter.com", "hubspot.net", "hubspot.com"];
+    const domainsToBlock = ["linkedin.com", "facebook.com", "twitter.com", "hubspot.net", "hubspot.com", "hsforms.com"];
     // Intercept and block network requests using Fetch API override
     (function () {
         const originalFetch = window.fetch;
